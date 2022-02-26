e32d43f17e708468a4f7794ac05db29030dd0b88
Tobi Oriola <44350099+majivcorp@users.noreply.github.com>
2022-02-26 17:26:45 -0500

    Update index.js
    function to pull real estate data from RapidApi - Bayut

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Link  from 'next/link';
 import Image from 'next/image';
 import { Flex, Box, Text, Button } from '@chakra-ui/react';
 
+import { baseUrl, fetchApi } from '../utils/fetchApi';
+
 const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, imageUrl }) => (
   <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
     <Image src={imageUrl} width={500} height={300} alt="banner" />
@@ -50,3 +52,7 @@ export default function Home() {
   )
 }
 
+export async function getStaticProps() {
+  const propertyForSale = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-sale&hitsPerpage=6`)
+  const propertyForRent = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-rent&hitsPerpage=6`)
+}
