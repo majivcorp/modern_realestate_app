c962c2a6fe9a086f719dd44898e9e38f707e65ce
Tobi Oriola <44350099+majivcorp@users.noreply.github.com>
2022-02-26 17:27:54 -0500

    Create fetchApi.js
    function to pull real estate data from RapidApi - Bayut

---
+++ utils/fetchApi.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+export const baseUrl = 'https://bayut.p.rapidapi.com'
+
+
+
+
+export const fetchApi = async (url) => {
+    const (data) = await axios.get((url), {
+        headers: {
+            'x-rapidapi-host': 'bayut.p.rapidapi.com',
+            'x-rapidapi-key': '04cf074643msh00d27b5cb457308p176ce2jsnc385a526df1a'
+        }  
+    })
+}
