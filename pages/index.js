c69fc803d01bbc10b8a08a4668f5372b504b9714
Tobi Oriola <44350099+majivcorp@users.noreply.github.com>
2022-02-26 19:32:13 -0500

    incremental changes

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,14 @@ import Link  from 'next/link';
 import Image from 'next/image';
 import { Flex, Box, Text, Button } from '@chakra-ui/react';
 
+import Property from '../components/Property';
 import { baseUrl, fetchApi } from '../utils/fetchApi';
 
 const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, imageUrl }) => (
   <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
     <Image src={imageUrl} width={500} height={300} alt="banner" />
     <Box p='5'>
-      <Text color="gray.500" fontSize="sm" fontWeight="medium"></Text>
+      <Text color="gray.500" fontSize="sm" fontWeight="medium">{purpose}</Text>
       <Text fontSize="3xl" fontWeight="bold">{title1}<br />{title2}</Text>
       <Text fontSize="lg" paddingTop="3" paddingBottom="3" color="gray.700">{desc1}<br />{desc2}</Text>
       <Button fontSize="xl" bg="blue.300">
@@ -18,10 +19,9 @@ const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, i
   </Flex>
 )
 
-export default function Home() {
+export default function Home( { propertiesForSale, propertiesForRent} ) {
   return(
     <box>
-     <h1>Hello World</h1>
      <Banner 
         purpose='Rent A Home'
         title1="Rental Homes for"
@@ -33,7 +33,7 @@ export default function Home() {
         imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
       />
       <Flex flexWrap="wrap">
-        {/*Fetch properties and map over them...*/}
+        {propertiesForRent.map((property) => <Property property={property} key={property.id} />)}
       </Flex>
      <Banner 
         purpose='Rent A Home'
@@ -46,7 +46,7 @@ export default function Home() {
         imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
       />
       <Flex flexWrap="wrap">
-        {/*Fetch properties and map over them...*/}
+      {propertiesForSale.map((property) => <Property property={property} key={property.id} />)}
       </Flex> 
     </box>
   )
@@ -55,4 +55,13 @@ export default function Home() {
 export async function getStaticProps() {
   const propertyForSale = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-sale&hitsPerpage=6`)
   const propertyForRent = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-rent&hitsPerpage=6`)
+
+  return {
+    props: {
+      propertiesForSale: propertyForSale?.hits,
+      propertiesForRent: propertyForRent?.hits,
+    }
+  }
+
+
 }
--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -6,10 +6,12 @@ export const baseUrl = 'https://bayut.p.rapidapi.com'
 
 
 export const fetchApi = async (url) => {
-    const (data) = await axios.get((url), {
+    const { data } = await axios.get((url), {
         headers: {
             'x-rapidapi-host': 'bayut.p.rapidapi.com',
             'x-rapidapi-key': '04cf074643msh00d27b5cb457308p176ce2jsnc385a526df1a'
-        }  
-    })
+        } 
+    });
+
+    return data;
 }
